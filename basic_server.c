47b3ea9bee7eca85fa6d8add3bde3cf0b03ce106
Zer0-M <msj2340@gmail.com>
2018-12-19 18:40:31 -0500

    testing cipher

--- basic_server.c
+++ basic_server.c
@@ -7,10 +7,22 @@ int main() {
   int from_client;
 
   from_client = server_handshake( &to_client );
-  printf("recreating well known-pipe");
-  char data[BUFFER_SIZE];
-  read(from_client,data,BUFFER_SIZE);
-  printf("The client says %s",data);
-  char *response="generic string";
-  write(to_client,response,strlen(response));
+  printf("recreating well known-pipe\n");
+  while(1){
+    char data[BUFFER_SIZE];
+    int bytesr=read(from_client,data,BUFFER_SIZE);
+    printf("%d\n",bytesr);
+    printf("The client says %s",data);
+    char response[strlen(data)];
+    /*for(int i=0;i<bytesr;i++){
+      if((int)data[i]+13>26){
+	response[i]=(char)((int)(data[i])-26+13);
+      }
+      else{
+	response[i]=(char)((int)(data[i])+13);
+      }
+    }*/
+    strcpy(response,data);
+    write(to_client,response,strlen(response));
+  }
 }
